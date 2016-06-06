cb8e62d3aca347687af4481e04ddb1a9c01e6c42
Rob Sears <secure@robsears.com>
2016-06-06 17:37:09 -0600

    Address some GitHub issues involving logger and runtime dependencies

--- bonsai-elasticsearch-rails.gemspec
+++ bonsai-elasticsearch-rails.gemspec
@@ -44,6 +44,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "elasticsearch-model", "~> 0"
+  spec.add_runtime_dependency "elasticsearch-rails", "~> 0"
   spec.add_development_dependency "bundler", "~> 1"
   spec.add_development_dependency "rake", "< 11.0"
 end
--- lib/bonsai-elasticsearch-rails.rb
+++ lib/bonsai-elasticsearch-rails.rb
@@ -1,13 +1,13 @@
 require 'elasticsearch/model'
-require 'logger'
+require 'elasticsearch/rails'
 
-url    = ENV['BONSAI_URL']
-logger = Logger.new(STDOUT)
+Rails.logger = Logger.new(STDOUT) if Rails.logger.nil?
+url    = ENV['BONSAI_URL'] || ENV['ELASTICSEARCH_URL']
 
 unless url.nil?
-  url_safe = url.gsub(/[a-z0-9]{1,}\:[a-z0-9]{1,}@/, "redacted:redacted@")
-  logger.info("Starting up a new Bonsai Elasticsearch client with url: #{url_safe}")
+  url_safe = url.gsub(/\:[a-z0-9]{1,}@/, ":redacted@")
+  Rails.logger.info("Starting up a new Bonsai Elasticsearch client with url: #{url_safe}")
   Elasticsearch::Model.client = Elasticsearch::Client.new url: url
 else
-  logger.warn('Could not find a `BONSAI_URL` environment variable with a cluster URL. The bonsai-elasticsearch-rails can not create an Elasticsearch client. Please contact support at: support@bonsai.io')
+  Rails.logger.warn('Could not find a `BONSAI_URL` environment variable with a cluster URL. The bonsai-elasticsearch-rails can not create an Elasticsearch client. Please contact support at: support@bonsai.io')
 end
